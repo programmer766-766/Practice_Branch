ed6bc56a059929bfd6babcd3824c103b3ba1fd70
Vijay balaji <balajibaskaran456@gmail.com>
2026-07-07 12:19:44 +0530

    Calculator feature implemented

---
+++ calculator.js
@@ -0,0 +1,10 @@
+let a=10;
+let b=20;
+console.log("calculator feature!!")
+
+
+console.log("Addition:"+a+b);
+console.log("Subtraction:"+a-b);
+console.log("Multiplication:"+a*b);
+console.log("Division:"+a/b);
+console.log("Module:"+a%b);
